406730b7809758f4b22f96dfb6ef0f3d1b58f449
Richard O'Neil <richard.oneil@garvin-allen.com>
2021-12-19 14:33:57 -0400

    add vimspector

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -94,6 +94,10 @@ return packer.startup(function(use)
   -- Git
   use "lewis6991/gitsigns.nvim"
 
+  -- Vimspector
+  use "puremourning/vimspector"
+  use "szw/vim-maximizer"
+
   -- Automatically set up your configuration after cloning packer.nvim
   -- Put this at the end after all plugins
   if PACKER_BOOTSTRAP then
