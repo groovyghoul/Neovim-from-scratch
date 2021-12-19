b7cfa5e2484465ca18cbeaaf5b6fb8456b62f9e9
Richard O'Neil <richard.oneil@garvin-allen.com>
2021-12-19 14:28:05 -0400

    personalize branding

--- lua/user/alpha.lua
+++ lua/user/alpha.lua
@@ -4,14 +4,18 @@ if not status_ok then
 end
 
 local dashboard = require("alpha.themes.dashboard")
+
 dashboard.section.header.val = {
-	[[                               __                ]],
-	[[  ___     ___    ___   __  __ /\_\    ___ ___    ]],
-	[[ / _ `\  / __`\ / __`\/\ \/\ \\/\ \  / __` __`\  ]],
-	[[/\ \/\ \/\  __//\ \_\ \ \ \_/ |\ \ \/\ \/\ \/\ \ ]],
-	[[\ \_\ \_\ \____\ \____/\ \___/  \ \_\ \_\ \_\ \_\]],
-	[[ \/_/\/_/\/____/\/___/  \/__/    \/_/\/_/\/_/\/_/]],
+ 	[[ _           _                                     ]],
+ 	[[| |         | |                                    ]],
+ 	[[| |__  _   _| |_ ___ _ __ ___   __ _ _ __ ___  ___ ]],
+ 	[[| '_ \| | | | __/ _ \ '_ ` _ \ / _` | '__/ _ \/ __|]],
+ 	[[| |_) | |_| | ||  __/ | | | | | (_| | | |  __/\__ \]],
+ 	[[|_.__/ \__, |\__\___|_| |_| |_|\__,_|_|  \___||___/]],
+ 	[[        __/ |                                      ]],
+ 	[[       |___/                                       ]],
 }
+
 dashboard.section.buttons.val = {
 	dashboard.button("f", "  Find file", ":Telescope find_files <CR>"),
 	dashboard.button("e", "  New file", ":ene <BAR> startinsert <CR>"),
@@ -28,7 +32,7 @@ local function footer()
 	-- local fortune = handle:read("*a")
 	-- handle:close()
 	-- return fortune
-	return "chrisatmachine.com"
+	return "bytemares.com"
 end
 
 dashboard.section.footer.val = footer()
