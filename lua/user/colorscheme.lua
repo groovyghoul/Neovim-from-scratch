74adeb78f5ef92144ee39d3b20e9af37df6f5c90
Richard O'Neil <richard.oneil@garvin-allen.com>
2022-01-03 18:48:59 -0400

    darken whichkey bg for landscape colorscheme

--- lua/user/colorscheme.lua
+++ lua/user/colorscheme.lua
@@ -1,6 +1,11 @@
 vim.cmd [[
 try
-  colorscheme landscape 
+  colorscheme landscape
+  
+  " the colorscheme landscape seems to throw off the color of whichkey
+  " this command darkens the background of the popup
+  highlight Pmenu guibg=#3A3A3A
+
 catch /^Vim\%((\a\+)\)\=:E185/
   colorscheme default
   set background=dark
