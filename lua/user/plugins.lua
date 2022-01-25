2e1edd7d85299590c8cd570a86aa3f0ffb652e0a
Richard O'Neil <richard.oneil@garvin-allen.com>
2022-01-25 09:57:57 -0400

    add nvim-notify

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -100,6 +100,18 @@ return packer.startup(function(use)
   use "puremourning/vimspector"
   use "szw/vim-maximizer"
 
+  use {
+    'rcarriga/nvim-notify',
+    config = function ()
+      require("notify").setup {
+        stages = 'fade_in_slide_out',
+        background_colour = 'FloatShadow',
+        timeout = 3000,
+      }
+      vim.notify = require('notify')
+    end
+  }
+
   -- Automatically set up your configuration after cloning packer.nvim
   -- Put this at the end after all plugins
   if PACKER_BOOTSTRAP then
