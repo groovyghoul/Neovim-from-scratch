9216ca090b8619316aee96eb0f1201ceb3946922
Richard O'Neil <richard.oneil@garvin-allen.com>
2022-02-08 20:02:10 -0400

    add nvim-tree-docs

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -91,6 +91,7 @@ return packer.startup(function(use)
     "nvim-treesitter/nvim-treesitter",
     run = ":TSUpdate",
   }
+  use "nvim-treesitter/nvim-tree-docs"
   use "JoosepAlviste/nvim-ts-context-commentstring"
 
   -- Git
