abb908d8e5e78ae994bd26353e705c6ce5996932
Richard O'Neil <richard.oneil@garvin-allen.com>
2022-02-08 22:29:42 -0400

    Increase notify timeout to 5 seconds

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -107,7 +107,7 @@ return packer.startup(function(use)
       require("notify").setup {
         stages = 'fade_in_slide_out',
         background_colour = 'FloatShadow',
-        timeout = 3000,
+        timeout = 5000,
       }
       vim.notify = require('notify')
     end
